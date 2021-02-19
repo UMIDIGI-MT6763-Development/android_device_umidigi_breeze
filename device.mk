a8c113a8d6fbe9d8c97eaba44ef2a5fa20f67e0d
Hadenix <vsegda_online_007@bk.ru>
2021-02-19 20:15:34 +0300

    breeze: Shim missing libshowlogo symbols
    - CANNOT LINK EXECUTABLE "kpoc_charger": cannot locate symbol "_ZN7android21SurfaceComposerClient17getBuiltInDisplayEi" referenced by "/system/lib/libshowlogo.so"
     * LineageOS/android_frameworks_native@dcb38bb#diff-b94da4d8daa8c8a6f8333b05522a8327L377
    
    - CANNOT LINK EXECUTABLE "kpoc_charger": cannot locate symbol "_ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EJjijPNS_14SurfaceControlEii" referenced by "/system/lib/libshowlogo.so"
     * P: https://github.com/LineageOS/android_frameworks_native/blob/lineage-16.0/libs/gui/SurfaceComposerClient.cpp#L616
     * Q: https://github.com/LineageOS/android_frameworks_native/blob/lineage-17.1/libs/gui/SurfaceComposerClient.cpp#L1316
    
    - CANNOT LINK EXECUTABLE "kpoc_charger": cannot locate symbol "_ZN7android14SurfaceControl5clearEv" referenced by "/system/lib/libshowlogo.so"
     * P: https://github.com/LineageOS/android_frameworks_native/blob/lineage-16.0/libs/gui/SurfaceControl.cpp#L77
     * Q: https://github.com/LineageOS/android_frameworks_native/blob/lineage-17.1/libs/gui/SurfaceControl.cpp#L74
    
    - CANNOT LINK EXECUTABLE "kpoc_charger": cannot locate symbol "_ZN7android4base10StartsWithERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPKc" referenced by "/system/lib/libshowlogo.so".
     * Just void it :3
    
     * KPOC is working fine now

--- device.mk
+++ device.mk
@@ -53,7 +53,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
-    
+
 # Init cripts
 PRODUCT_PACKAGES += \
     init.mt6763.rc \
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.xml
+
+# Symbols 
+PRODUCT_PACKAGES += \
+    libshim_showlogo
 
 # Telephony Jars
 PRODUCT_BOOT_JARS += \
